d4473a9fd28005272768198b59980b96486a8d5c
sunjing <sunjing@mzhcorp.com>
2022-11-15 15:36:51 +0800

    chore: ahooks

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import { hot } from "react-hot-loader/root";
+
+function App() {
+  return (
+    <>
+      {/* <TryDrag /> */}
+
+      {/* SideEffect */}
+      {/* <TryDebounce /> */}
+    </>
+  )
+}
+
+export default hot(App);
