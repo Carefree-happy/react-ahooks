67483bd1055aa3a408d01ed2f38e2558d365158b
sunjing <sunjing@mzhcorp.com>
2022-11-16 13:14:03 +0800

    feat: refreshOnWindowFocus

---
+++ src/useRequest/0600RefreshOnWindowFocus.tsx
@@ -0,0 +1,21 @@
+import Mock from 'mockjs';
+import * as React from 'react';
+import { useRequest } from 'ahooks';
+
+function getUsername() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(Mock.mock('@name'));
+        }, 1000);
+    });
+}
+
+const RefreshOnWindowFocus = () => {
+    const { data, loading } = useRequest(getUsername, {
+        refreshOnWindowFocus: true,
+    });
+
+    return <div>Username: <>{loading ? 'Loading' : data}</></div>;
+};
+
+export default RefreshOnWindowFocus 
