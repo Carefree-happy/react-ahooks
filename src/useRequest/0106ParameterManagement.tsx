0db68bc14a9710c5ec1587adf9ecdaa544fb0e00
sunjing <sunjing@mzhcorp.com>
2022-11-16 13:11:10 +0800

    feat: parameterManagement

---
+++ src/useRequest/0106ParameterManagement.tsx
@@ -0,0 +1,47 @@
+import { useRequest } from 'ahooks';
+import Mock from 'mockjs';
+import * as React from 'react';
+import { useState } from 'react';
+
+function getUsername(id: string): Promise<string> {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(Mock.mock('@name'));
+        }, 1000);
+    });
+}
+
+const ParameterManagement = () => {
+    const [state, setState] = useState('');
+
+    // get username
+    const {
+        data: username,
+        run,
+        params,
+    } = useRequest(getUsername, {
+        defaultParams: ['1'],
+    });
+
+    const onChange = () => {
+        run(state);
+    };
+
+    return (
+        <div>
+            <input
+                onChange={(e) => setState(e.target.value)}
+                value={state}
+                placeholder="Please enter userId"
+                style={{ width: 240, marginRight: 16 }}
+            />
+            <button type="button" onClick={onChange}>
+                GetUserName
+            </button>
+            <p style={{ marginTop: 8 }}>UserId: {params[0]}</p>
+            <p>Username: {username}</p>
+        </div>
+    );
+};
+
+export default ParameterManagement
