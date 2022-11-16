b921bf22f26dc9dc06c433b4764c90dc4739931d
sunjing <sunjing@mzhcorp.com>
2022-11-16 13:15:31 +0800

    feat: keepYourDataFresh

---
+++ src/useRequest/0901KeepYourDataFresh.tsx
@@ -0,0 +1,47 @@
+import { useBoolean } from 'ahooks';
+import Mock from 'mockjs';
+import * as React from 'react';
+import { useRequest } from 'ahooks';
+
+async function getArticle(): Promise<{ data: string; time: number }> {
+    console.log('cacheKey-staleTime');
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve({
+                data: Mock.mock('@paragraph'),
+                time: new Date().getTime(),
+            });
+        }, 1000);
+    });
+}
+
+const Article = () => {
+    const { data, loading } = useRequest(getArticle, {
+        cacheKey: 'staleTime-demo',
+        staleTime: 5000,
+    });
+    if (!data && loading) {
+        return <p>Loading</p>;
+    }
+    return (
+        <>
+            <p>Background loading: {loading ? 'true' : 'false'}</p>
+            <p>Latest request time: {data?.time}</p>
+            <p>{data?.data}</p>
+        </>
+    );
+};
+
+const KeepYourDataFresh = () => {
+    const [state, { toggle }] = useBoolean();
+    return (
+        <div>
+            <button type="button" onClick={() => toggle()}>
+                show/hidden
+            </button>
+            {state && <Article />}
+        </div>
+    );
+};
+
+export default KeepYourDataFresh
