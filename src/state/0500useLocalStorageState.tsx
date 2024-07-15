4be7d9f6c6915adeb92fb6679228df3b47465714
Carefree-happy <2507057523@qq.com>
2024-07-16 00:38:59 +0800

    feat: LocalStorageState

---
+++ src/state/0500useLocalStorageState.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { useLocalStorageState } from 'ahooks';
+
+function LocalStorageState () {
+  const [message, setMessage] = useLocalStorageState<string | undefined>(
+    'use-local-storage-state-demo1',
+    {
+      defaultValue: 'Hello~',
+    },
+  );
+
+  return (
+    <>
+      <input
+        value={message || ''}
+        placeholder="Please enter some words..."
+        onChange={(e) => setMessage(e.target.value)}
+      />
+      <button style={{ margin: '0 8px' }} type="button" onClick={() => setMessage('Hello~')}>
+        Reset
+      </button>
+      <button type="button" onClick={() => setMessage(undefined)}>
+        Clear
+      </button>
+    </>
+  );
+}
+
+export default LocalStorageState;
