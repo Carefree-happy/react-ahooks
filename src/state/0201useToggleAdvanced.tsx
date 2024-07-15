bbcc92fde7c9eda8ae521edd632888524f320df8
Carefree-happy <2507057523@qq.com>
2024-07-16 00:38:35 +0800

    feat: ToggleAdvanced

---
+++ src/state/0201useToggleAdvanced.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { useToggle } from 'ahooks';
+
+function ToggleAdvanced () {
+  const [state, { toggle, setLeft, setRight }] = useToggle();
+
+  return (
+    <div>
+      <p>Effects：{`${state}`}</p>
+      <p>
+        <button type="button" onClick={toggle}>
+          Toggle
+        </button>
+        <button type="button" onClick={setLeft} style={{ margin: '0 8px' }}>
+          Toggle False
+        </button>
+        <button type="button" onClick={setRight}>
+          Toggle True
+        </button>
+      </p>
+    </div>
+  );
+};
+
+export default ToggleAdvanced;
