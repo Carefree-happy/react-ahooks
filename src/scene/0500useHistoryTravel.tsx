2a7ade8e2e7a8659133fbb7ddcea69f6ec408d53
Carefree-happy <2507057523@qq.com>
2024-07-15 17:52:58 +0800

    feat: historyTravel

---
+++ src/scene/0500useHistoryTravel.tsx
@@ -0,0 +1,20 @@
+import { useHistoryTravel } from 'ahooks';
+import React from 'react';
+
+function BasicUsage() {
+  const { value, setValue, backLength, forwardLength, back, forward } = useHistoryTravel<string>();
+
+  return (
+    <div>
+      <input value={value || ''} onChange={(e) => setValue(e.target.value)} />
+      <button disabled={backLength <= 0} onClick={back} style={{ margin: '0 8px' }}>
+        back
+      </button>
+      <button disabled={forwardLength <= 0} onClick={forward}>
+        forward
+      </button>
+    </div>
+  );
+};
+
+export default BasicUsage
