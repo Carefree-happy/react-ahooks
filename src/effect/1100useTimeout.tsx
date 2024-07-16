7c956ca7aae713b574659d89ddf2382eb4be3365
Carefree-happy <2507057523@qq.com>
2024-07-16 15:18:24 +0800

    feat: Timeout

---
+++ src/effect/1100useTimeout.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import { useTimeout } from 'ahooks';
+
+function Timeout() {
+  const [count, setCount] = useState(0);
+  const [delay, setDelay] = useState<number | undefined>(1000);
+
+  const clear = useTimeout(() => {
+    setCount(count + 1);
+  }, delay);
+
+  return (
+    <div>
+      <p> count: {count} </p>
+      <p style={{ marginTop: 16 }}> Delay: {delay} </p>
+      <button onClick={() => setDelay((t) => (!!t ? t + 1000 : 1000))} style={{ marginRight: 8 }}>
+        Delay + 1000
+      </button>
+      <button
+        style={{ marginRight: 8 }}
+        onClick={() => {
+          setDelay(1000);
+        }}
+      >
+        reset Delay
+      </button>
+      <button onClick={clear}>clear</button>
+    </div>
+  );
+};
+
+export default Timeout;
