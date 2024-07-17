3a80bfdd35c642471fca8d20858f6a928df47327
Carefree-happy <2507057523@qq.com>
2024-07-18 00:09:25 +0800

    feat: Mouse

---
+++ src/dom/1300useMouse.tsx
@@ -0,0 +1,22 @@
+import { useMouse } from 'ahooks';
+import React from 'react';
+
+const Mouse = () => {
+  const mouse = useMouse();
+
+  return (
+    <div>
+      <p>
+        Client - x: {mouse.clientX}, y: {mouse.clientY}
+      </p>
+      <p>
+        Page - x: {mouse.pageX}, y: {mouse.pageY}
+      </p>
+      <p>
+        Screen - x: {mouse.screenX}, y: {mouse.screenY}
+      </p>
+    </div>
+  );
+};
+
+export default Mouse;
