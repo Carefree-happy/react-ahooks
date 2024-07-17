899ba3adf7757394a7d713a343eb59bd42b81e5e
Carefree-happy <2507057523@qq.com>
2024-07-17 23:59:06 +0800

    feat: KeyPressTrigger

---
+++ src/dom/1104useKeyPressTrigger.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import { useKeyPress } from 'ahooks';
+
+const KeyPressTrigger = () => {
+  const [count, setCount] = useState<number>(0);
+
+  const keyCallbackMap = {
+    w: () => {
+      setCount((prev) => prev + 1);
+    },
+    s: () => {
+      setCount((prev) => prev - 1);
+    },
+    'shift.c': () => {
+      setCount(0);
+    },
+  } as any;
+
+  useKeyPress(['w', 's', 'shift.c'], (e, key) => {
+    keyCallbackMap[key]();
+  });
+
+  return (
+    <div>
+      <p>Try pressing the following: </p>
+      <div>1. Press [w] to increase</div>
+      <div>2. Press [s] to decrease</div>
+      <div>3. Press [shift.c] to reset</div>
+      <p>
+        counter: <span style={{ color: '#f00' }}>{count}</span>
+      </p>
+    </div>
+  );
+};
+
+export default KeyPressTrigger;
